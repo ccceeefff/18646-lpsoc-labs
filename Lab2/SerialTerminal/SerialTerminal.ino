dc73baf28feae2eaa24e78d40968720c35e3ce62
Cef Ramirez <cef@Cefs-MacBook-Pro.local>
2016-02-09 11:00:27 -0800

    extracting command and arguments

--- Lab2/SerialTerminal/SerialTerminal.ino
+++ Lab2/SerialTerminal/SerialTerminal.ino
@@ -1,16 +1,64 @@
-String command = "";
+#define MAX_ARG_COUNT 5
+String inputBuffer = "";
+const char *argv[MAX_ARG_COUNT]; // support up to 5 parameters
+int argc = 0;
+
+void interpret(String command, const char *params[], int paramCount){
+  SerialUSB.println("command: " + command);
+  if(paramCount > 0){
+    SerialUSB.println("parameters: ");
+    for(int i=0; i<paramCount; i++){
+      SerialUSB.println(params[i]);
+    }
+  }
+}
+
+void parse(String line){
+  SerialUSB.println(line);
+  int space = line.indexOf(" ");
+  String command;
+  if(space != 0){
+    command = line.substring(0, space);
+  } else {
+    command = line;
+  }
+
+  // remove command from line
+  line.remove(0, command.length());
+  line.trim();
+  
+  // extract arguments
+  const char *args = line.c_str();
+  argc = 0;
+  int start = 0;
+  space = 0;
+  while( (start < line.length()) && argc < MAX_ARG_COUNT){
+    space = line.indexOf(" ", space);
+    argv[argc] = &args[start];
+    argc++;
+    if(space != -1){ // more arguments later
+      ((char *)args)[space] = '\0'; // add null terminator here
+      start = space = space+1;
+    } else { // we've reached the end
+      break;
+    }
+  }
+
+  interpret(command, argv, argc);
+}
 
 void setup() {
-   // setup SerialUSB
-   SerialUSB.begin(115200);
-   while(!SerialUSB);
+  // setup SerialUSB
+  SerialUSB.begin(115200);
+  while(!SerialUSB);
 }
 
 void loop() {
   SerialUSB.print("cmd> ");
   while(!SerialUSB.available());
-  command = SerialUSB.readStringUntil('\n');
-  if(command.length() > 0){
-    SerialUSB.println(command);
+  inputBuffer = SerialUSB.readStringUntil('\n');
+  if(inputBuffer.length() > 0){
+    inputBuffer.trim();
+    parse(inputBuffer);
   }
 }
