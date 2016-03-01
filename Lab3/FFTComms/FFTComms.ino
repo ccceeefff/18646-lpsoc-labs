e82dc73356c1c7c0ce20f6dc54e2e2e696bf46c6
Cef Ramirez <cef@Cefs-MacBook-Pro.local>
2016-03-01 01:25:04 -0800

    adding wifi/serial submission

--- Lab3/FFTComms/FFTComms.ino
+++ Lab3/FFTComms/FFTComms.ino
@@ -1,12 +1,32 @@
 #include <arduinoFFT.h>
 #include <FreeRTOS_ARM.h>
+#include <WiFiEsp.h>
 
 #include "FFTAudioSampler.h"
+#include "FFTConfig.h"
 
-#define AUDIO_BUFFER_QUEUE_SIZE 5
+#if NET_SUBMIT
+WiFiEspClient client;
+#else
+#define client SerialUSB
+#endif
+
 QueueHandle_t xAudioBufferQueue;
+QueueHandle_t xSenderQueue;
 
 arduinoFFT FFT = arduinoFFT();
+
+char wifi_ssid[] = "CMU-SV";            // your network SSID (name)
+char wifi_pass[] = "";        // your network password
+
+char server_host[] = "10.0.15.226";
+int server_port = 80;
+
+#if LOCAL_FFT
+char server_endpoint[] = "/CefRamirez/fft";
+#else
+char server_endpoint[] = "/CefRamirez/signal";
+#endif
 
 double vReal[NUM_SAMPLES];
 double vImag[NUM_SAMPLES];
@@ -26,25 +46,82 @@ static void Task_FFT(void *arg){
           vReal[i] = audioBuffer[i];
           vImag[i] = 0;
         }
+        
         // compute FFT
-        if(LOCAL_FFT){
+#if LOCAL_FFT
           FFT.Windowing(vReal, NUM_SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
           FFT.Compute(vReal, vImag, NUM_SAMPLES, FFT_FORWARD); /* Compute FFT */
           FFT.ComplexToMagnitude(vReal, vImag, NUM_SAMPLES); /* Compute magnitudes */
           double x = FFT.MajorPeak(vReal, NUM_SAMPLES, SAMPLING_RATE);
-          
           SerialUSB.print("major peak: ");
           SerialUSB.println(x);
-        }
+#else
+          SerialUSB.println("Buffer full.. skipping FFT computation..");          
+#endif
+
+        // transmit packet
+        xQueueSendToBack(xSenderQueue, &vReal, portMAX_DELAY);
       }
+      audioBuffer = NULL;
     }
   }
 }
 
-static void Task_SerialOutGateKeeper(void *arg){
-  uint16_t buffer = 0;
+static void Task_Sender(void *arg){
+  double *values = NULL;
   while(1){
-    taskYIELD();
+    if(xQueueReceive(xSenderQueue, &values, portMAX_DELAY)){
+      SerialUSB.println("Sending packet....");
+#if NET_SUBMIT
+        if(client.connect(server_host, server_port)){
+          // send post request headers
+          SerialUSB.println("Connected to server...");
+          client.print("POST ");
+          client.print(server_endpoint);
+          client.println(" HTTP/1.1");
+          client.println("Content-Type: application/json");
+          client.println("Connection: close");
+          client.println();
+#endif         
+          // send json encoded data
+          client.print("{\"data\":[");
+          for(int i=0; i < NUM_SAMPLES; i++){
+            client.print("[");
+            client.print(i);
+            client.print(",");
+            client.print(vReal[i]);
+            client.print("]");
+            if(i < NUM_SAMPLES-1){
+              client.print(",");
+            }
+          }
+          client.print("]");
+#if !LOCAL_FFT
+          client.print(", \"sampleRate\": ");
+          client.print(SAMPLING_RATE);
+#endif
+          client.println("}");
+          client.println();
+          
+#if NET_SUBMIT
+          // flush response
+          while(client.available()){
+            char c = client.read();
+            SerialUSB.write(c);
+          }
+
+          if(!client.connected()){
+            SerialUSB.println();
+            SerialUSB.println("Disconnecting from server...");
+            client.stop();
+          }
+        } // close client connection
+        else {
+          SerialUSB.println("Failed to connect to server.");
+        }
+#endif
+      
+    }
   }
 }
 
@@ -53,11 +130,18 @@ void setup() {
   while(!SerialUSB);
   SerialUSB.println("Ready!");
 
+#if NET_SUBMIT
+  initializeWiFi();
+#else
+  client = SerialUSB;  
+#endif
+
   AudioSampler.setBufferReadyCallback(buffer_ready);
 
-  xAudioBufferQueue = xQueueCreate(AUDIO_BUFFER_QUEUE_SIZE, sizeof(uint16_t *));
+  xAudioBufferQueue = xQueueCreate(AUDIO_BUFFER_QUEUE_SIZE, sizeof(double *));
+  xSenderQueue = xQueueCreate(SENDER_QUEUE_SIZE, sizeof(double *));
 
-  // xTaskCreate(Task_SerialOutGateKeeper, NULL, configMINIMAL_STACK_SIZE, NULL, 0, NULL);
+  xTaskCreate(Task_Sender, NULL, configMINIMAL_STACK_SIZE, NULL, 0, NULL);
   xTaskCreate(Task_FFT, NULL, configMINIMAL_STACK_SIZE, NULL, 0, NULL);
 
   SerialUSB.println("Tasks ready!");
@@ -74,4 +158,48 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+/* Utility Methods */
+void initializeWiFi()
+{
+  PDWiFiSerial.begin(PDWiFiSerialBaudRate);
+  WiFi.init(&PDWiFiSerial);
+
+  if (WiFi.status() == WL_NO_SHIELD){
+    SerialUSB.println("WiFi shield not present");
+    while(true);
+  }
+
+  int status = WL_IDLE_STATUS;
+  
+  while(status != WL_CONNECTED){
+    SerialUSB.print("Attempting to connect to WPA SSID: ");
+    SerialUSB.println(wifi_ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(wifi_ssid, wifi_pass);
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+
+  printWifiStatus();
+}
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  SerialUSB.print("SSID: ");
+  SerialUSB.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  SerialUSB.print("IP Address: ");
+  SerialUSB.println(ip);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  SerialUSB.print("Signal strength (RSSI):");
+  SerialUSB.print(rssi);
+  SerialUSB.println(" dBm");
 }
--- Lab3/FFTComms/FFTConfig.h
+++ Lab3/FFTComms/FFTConfig.h
@@ -2,12 +2,23 @@
 #define FFTCONFIG_H
 
 #define NUM_BUFFERS 2
-#define NUM_SAMPLES 128
+#define NUM_SAMPLES 16
 
-#define SAMPLING_RATE 20
+#define SAMPLING_RATE 100
 
 #define ADC_PORT 0
 
+#define AUDIO_BUFFER_QUEUE_SIZE 5
+#define SENDER_QUEUE_SIZE 5
+
 #define LOCAL_FFT 1
+
+/*
+ * WiFi ESP8266 Properties
+ */
+#define NET_SUBMIT 0
+ 
+#define PDWiFiSerial Serial1
+#define PDWiFiSerialBaudRate 9600
 
 #endif //FFTCONFIG_H
